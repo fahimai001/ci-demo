2c976b2eea2188cfaef01832936c7e5b7b3129e5
fahimai001 <fahimsabircstec001@gmail.com>
2024-12-04 02:19:02 -0800

    Resolve minor error

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-def add(a,b):
-    
-    return a+b
+def add(a, b):
+    return a + b
 
 
-
-
-
-
-def sub(a,b):return a-b
+def sub(a, b):
+    return a - b
 
 result1 = add(1,2)
 result2 = sub(5,6)
+
 print(result1,result2)
